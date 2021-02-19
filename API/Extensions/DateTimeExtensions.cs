1fc82dc7d7dff602b0046bfff611af87b03cd91c
Vivek-Baliyan <38033756+Vivek-Baliyan@users.noreply.github.com>
2021-02-20 00:46:01 +0530

    End of section 8

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateTime dob)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - dob.Year;
+            if (dob.Date > today.AddYears(-age)) age--;
+            return age;
+
+        }
+    }
+}
